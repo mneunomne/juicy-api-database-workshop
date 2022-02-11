b48eb29fcf65cecaec5c833e9192c7ac757e7381
mneu windows <alberto.harres@gmail.com>
2022-02-11 20:20:58 +0100

    imelpement everything

--- public/script.js
+++ public/script.js
@@ -1,27 +1,86 @@
-// simple get request
-$.get("/api/texts", function( data ) {
-  // iterate for each datapoint in the database
-  updateData(data)
-});
+const socket = io();
 
-function updateData (messages) {
+$( document ).ready(init)
+
+function init () {
+  getMessages()
+}
+
+function getMessages () {
+  // simple get request
+  $.get("/api/texts", function( texts ) {
+    // iterate for each datapoint in the database
+    updateData(texts)
+  });
+}
+
+function updateData (texts) {
   $( ".result" ).empty()
-  for (var i in messages) {
-    console.log(messages[i].message);
-    $( ".result" ).append( `<p>${messages[i].message}</p>` );
+  for (var i in texts) {
+    addMessage(texts[i])
   }
+  setInteract()
+}
+
+function addMessage (data) {
+  var id = data.id
+  var x = data.x
+  var y = data.y
+  var message = data.message
+  $( ".result" ).append(`
+    <div id="${id}" style="left: ${x}%; top: ${y}%;" class="draggable">
+      <span>${message}</span>
+    </div>
+  `);
+}
+
+function updateMessage (data) {
+  $(`#${data.id}`).css("left", data.x + "%");
+  $(`#${data.id}`).css("top", data.y + "%");
+}
+
+function deleteMessage (id) {
+  $.ajax({
+    type: 'DELETE',
+    url: "/api/text",   
+    contentType: 'application/json',
+    data:  JSON.stringify({
+      id: id
+    })
+  })
+}
+
+function changeMessagePosition (id, x, y) {
+  $.ajax({
+    type: 'put',
+    url: "/api/text",   
+    contentType: 'application/json',
+    data:  JSON.stringify({
+      x: x,
+      y: y,
+      id: id
+    })
+  })
 }
 
 
 function submitText (text) {
+  var x = Math.random() * 100
+  var y = Math.random() * 100
+  var id = guid()
   $.ajax({
     type: 'post',
     url: "/api/text",   
     contentType: 'application/json',
-    data:  JSON.stringify({message: text}),
+    data:  JSON.stringify({
+      id: id,
+      x: x,
+      y: y,
+      message: text
+    }),
     success: function (data) {
-      console.log("done", data)
-      updateData(data)
+      console.log("done")
+      // updateData(data)
     },
     error: function () {
       console.log('We are sorry but our servers are having an issue right now');
@@ -29,8 +88,58 @@ function submitText (text) {
   })
 }
 
+function setInteract () {
+  $( ".draggable" ).draggable({
+    containment: ".result",
+    start: function (evt) {
+      console.log("start", evt, evt.target.offsetLeft, evt.target.offsetTop)
+      this.startX = evt.target.offsetLeft
+      this.startY = evt.target.offsetTop
+    },
+    stop: function (evt) {
+      console.log("end", evt, this.startX, this.startY, evt.offsetX, evt.offsetY)
+      var x = ( 100 * (parseFloat(evt.target.offsetLeft) / parseFloat($(this).parent().width())) )
+      var y = ( 100 * (parseFloat(evt.target.offsetTop) / parseFloat($(this).parent().height())) )
+      console.log("end 2 ", this.startX + evt.offsetX, this.startY + evt.offsetY)
+      $(this).css("left", x + "%");
+      $(this).css("top", y + "%");
+      changeMessagePosition(evt.target.id, x, y)
+    }
+  });
+
+  $( ".draggable" ).dblclick(function (evt) {
+    console.log("delete", $(this)[0].id)
+    deleteMessage($(this)[0].id)
+    $(this).remove()
+  })
+}
+
+socket.on('message_added', function(data) {
+  addMessage(data)
+  setInteract()
+});
+
+socket.on('message_updated', function(data) {
+  updateMessage(data)
+  console.log("data", data)
+});
+
+socket.on('message_deleted', function(id) {
+  $(`#${id}`).remove()
+});
+
 $('#submit').click(function (evt) {
   console.log('text',  $("#text").val())
   submitText($("#text").val())
   $("#text").val('')
 })
+
+
+let guid = () => {
+  let s4 = () => {
+      return Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
--- server.js
+++ server.js
@@ -2,9 +2,23 @@
 const express = require('express')
 const mongoose = require("mongoose")
 const bodyParser = require('body-parser')
+const { Server } = require("socket.io");
+const http = require('http')
+require('dotenv').config()
+
+const PORT = process.env.PORT || 3000
 
 // server application 
-var app = express()
+const app = express()
+const server = http.Server(app);
+
+// socket.io
+const io = new Server(server);
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+// body parser (to correctly intepret the messages coming from the "POST" request)
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
@@ -15,7 +29,10 @@ const mongoUri = `mongodb+srv://moin:juicy123@cluster0.n8go0.mongodb.net/text-bo
 
 // MongoDB User Schema 
 const Text = mongoose.model('Text', mongoose.Schema({
-  message: String
+  id: String,
+  message: String,
+  x: Number,
+  y: Number
 }));
 
 mongoose.connect(mongoUri, { useNewUrlParser: true }, function (err, res) {
@@ -42,20 +59,58 @@ app.get('/api/texts', function (req, res) {
 // submit new text
 app.post('/api/text', function (req, res) {
   console.log('request', req.body)
+  var id = req.body.id
   var message = req.body.message
-  new Text({
+  var x = parseFloat(req.body.x)
+  var y = parseFloat(req.body.y)
+  var data = {
+    id: id,
     message: message,
-  }).save(function (err) { // save the message on the database
+    x: x,
+    y: y
+  } 
+  new Text(data).save(function (err) { // save the message on the database
     if (err) return console.error(err);
     console.log(`[MongoDB] saved to ${message} database!`,)
     Text.find({}, function (err, texts) {
       if (err) console.error(err)
+      // send update to everyone
+      io.emit('message_added', data); // This will emit the event to all connected sockets
+      // return the new array to the post request 
       res.json(texts)
     })
   });
 })
 
+app.put('/api/text', function (req, res) {
+  var id = req.body.id
+  var x = parseFloat(req.body.x)
+  var y = parseFloat(req.body.y)
+  var data = {id, x, y}
+  const filter = {"id": id}
+  const update = { $set :{ "x": x, "y": y } };
+  let doc = Text.findOneAndUpdate(filter, update, {
+    upsert: true,
+  }, function(err, doc) {
+    if (err) return res.send(500, {error: err});
+    io.emit('message_updated', data); // This will emit the event to all connected sockets
+    return res.send('Succesfully saved.');
+  }); 
+})
+
+app.delete('/api/text', function (req, res) {
+  var id = req.body.id
+  console.log("id", id)
+  const filter = {"id": id}
+  Text.deleteOne(filter, function (err) {
+    if (err) return handleError(err);
+    console.log("document deleted")
+    io.emit('message_deleted', id);
+    // deleted at most one tank document
+  });
+})
+
 // setting the server to run on port 3000
-app.listen(3000, function () {
-  console.log('Listening on port 3000...')
+server.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}...`)
 })
